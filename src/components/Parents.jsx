9d238c2a27e89b259897c14ca4d2446717aa336f
Lavi-debug <acelavi166@gmail.com>
2024-11-06 15:30:28 +0530

    new layout

--- src/components/Parents.jsx
+++ src/components/Parents.jsx
@@ -116,45 +116,70 @@ const Parents = ({ license, contact }) => {
             ref={mapRef}
             style={{ height: '100%', width: '100%' }}
           ></div>
+
           <div className="list flex-grow h-[40%]">
             <div className="relative shadow-md rounded-bl-lg rounded-br-lg h-full overflow-y-auto scrollbar-hidden">
               <div className="w-full text-xs md:text-sm text-gray-500">
                 <div className="flex bg-gray-50 text-gray-700 uppercase text-center font-semibold">
-                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-3">Student</div>
-                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-3">School</div>
-                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-3">Residence</div>
-                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-3">Pt.Contact</div>
-                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-3">Status</div>
-                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-3">Time</div>
+
+                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
+                    <div className="w-[100px] break-words text-center">Student</div>
+                  </div>
+
+                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
+                    <div className="w-[100px] break-words text-center">School</div>
+                  </div>
+
+                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
+                    <div className="w-[100px] break-words text-center">Status</div>
+                  </div>
+
+                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
+                    <div className="w-[100px] break-words text-center">Time</div>
+                  </div>
+
+                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
+                    <div className="w-[100px] break-words text-center">Residence</div>
+                  </div>
+
+                  <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
+                    <div className="w-[100px] break-words text-center">Pt.Contact</div>
+                  </div>
+                  
                 </div>
 
                 {filteredStudents.map((student, index) => (
                   <div key={student.id} className={`flex ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'} border-b`}>
+
                     <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center text-gray-900 font-medium">
                       <div className="w-[100px] break-words text-center">{student.Child}</div>
                     </div>
+
                     <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
                       <div className="w-[100px] break-words text-center">{student.School}</div>
                     </div>
-                    <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
-                      <div className="w-[100px] break-words text-center">{student.Address}</div>
-                    </div>
-                    <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
-                      <div className="w-[100px] break-words text-center">{student.Contact}</div>
-                    </div>
+
                     <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
                       <div className="w-[100px] border border-gray-300 rounded py-1 break-words text-center">
                         <h1 className="text-[10px] md:text-xs font-medium">{student.Status}</h1>
                       </div>
                     </div>
+
                     <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
                       <div className="w-[120px] text-center">{student.Time}</div>
                     </div>
+                    
+                    <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
+                      <div className="w-[100px] break-words text-center">{student.Address}</div>
+                    </div>
+                    
+                    <div className="w-1/4 px-2 py-3 md:px-6 md:py-4 flex justify-center items-center">
+                      <div className="w-[100px] break-words text-center">{student.Contact}</div>
+                    </div>
+                    
                   </div>
                 ))}
               </div>
-
-
 
             </div>
           </div>
